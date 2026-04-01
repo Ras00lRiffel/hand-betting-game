4a0eca3f1d5c3fd9b46518e8aeee9ce630b86d6b
Rasool Riffel <riffelrasool@gmail.com>
2026-04-01 10:17:14 +0300

    #HBG: Adding the landing page for the game

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Landing} from './features/landing/landing';
+// import { GameComponent } from './features/game/game.component';
+// import { LeaderboardComponent } from './features/leaderboard/leaderboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing },
+//   { path: 'game', component: GameComponent },
+];
